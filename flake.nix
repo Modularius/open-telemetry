34a9e6c38b5bdbd6dc1cfe62ebe9c6629b6f749e
Modularius <daniel.kirk@rocketmail.com>
2025-03-16 16:52:20 +0000

    First commit

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    pipeline.url = "github:STFC-ICD-Research-and-Design/supermusr-data-pipeline";
+    #pipeline.url = "/home/ubuntu/SuperMuSRDataPipeline?dir=supermusr-data-pipeline";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pipeline
+  } : flake-utils.lib.eachDefaultSystem
+    ( system:
+        let
+          pkgs = (import nixpkgs) {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nil
+              nixd
+              direnv
+              python312
+              valgrind-light
+              cifs-utils
+              nfs-utils
+              hdf5_1_10
+              kcat
+            ] ++ (
+              with python312Packages; [
+                pip
+                requests
+                pandas
+                matplotlib
+                numpy
+                scipy
+                ipykernel
+                ipywidgets
+                h5py
+                elasticsearch
+              ]
+            );
+            inputsFrom  = [
+              pipeline.devShell.${system}
+            ];
+          };
+        }
+    );
+}
